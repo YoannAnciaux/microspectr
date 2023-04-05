346062a910ea876c6be1e2d0752bfee8f1fa8d4e
YoannAnciaux <anciaux.yoann@gmail.com>
2023-04-06 00:57:10 +0200

    MAJ dependencies

--- R/microspectr-package.R
+++ R/microspectr-package.R
@@ -3,9 +3,14 @@
 
 ## usethis namespace: start
 #' @import dplyr
+#' @importFrom purrr map_dbl
 #' @importFrom readxl read_excel
 #' @importFrom rlang set_names
+#' @importFrom segmented segmented
 #' @importFrom stringr str_replace_all
+#' @importFrom tidyr pivot_longer
+#' @importFrom tidyr pivot_wider
 #' @importFrom tidyr separate
+#' @importFrom tidyr unite
 ## usethis namespace: end
 NULL
