babade6581f664f6830418ea6acb520549219f29
YoannAnciaux <anciaux.yoann@gmail.com>
2023-04-06 00:54:49 +0200

    Correct var in tidyverse - Variable names in tidyvers are nod declared and considred as undefined global so we need to add .data$ before which is deprecated in tidyverse...

--- R/import_data.R
+++ R/import_data.R
@@ -17,20 +17,19 @@
 #' Signal contains either raw names from *BMG MARS* or new names from `name_repair`.
 #' @export
 #' @examples
-#' datasets <- microspectr_example("Ecoli_T7_24well_Lux_2Fluo_OD.xlsx")
-#' import_excel(datasets)
+# datasets <- microspectr_example("Ecoli_T7_24well_Lux_2Fluo_OD.xlsx")
+# import_excel(datasets)
 #'
 #' # With repaired names
 #' signal_names <- list("No filter 1" = "LUX", "480-20 2" = "CFP", "600-40 3" = "SYTOX", "4" = "OD")
 #' import_excel(datasets, name_repair = signal_names)
-
 import_excel <- function(path,
                          name_repair = NULL) {
   data <- suppressMessages(read_excel(path))
   # Select column names which do not correspond to signal values
   discriminatory_variable <- select(data, !starts_with("Raw Data")) %>% colnames()
   # Extract column names of the signal associated with the time at which the signal has been measured
-  tbl_name_time <- filter(data, grepl("Time", Content)) %>%
+  tbl_name_time <- filter(data, grepl("Time", .data$Content)) %>%
     select(starts_with("Raw Data")) %>%
     pivot_longer(cols = everything(),
                  names_to = "Signal",
@@ -40,19 +39,19 @@ import_excel <- function(path,
     pattern <- paste0(".*(\\(", names(name_repair), "\\)).*") # pattern to recognize the names from name_repair between parentheses
     replacement <- unname(unlist(name_repair))
     tbl_name_time <- tbl_name_time %>%
-      mutate(raw_Signal = Signal,
-             Signal = str_replace_all(Signal, pattern = set_names(replacement, pattern)))
+      mutate(raw_Signal = .data$Signal,
+             Signal = str_replace_all(.data$Signal, pattern = set_names(replacement, pattern)))
   } else {
     tbl_name_time <- tbl_name_time %>%
-      mutate(raw_Signal = Signal,
-             Signal = gsub(".*\\((.*)\\).*", "\\1", Signal)) # pattern to extract the names that are inside the parentheses
+      mutate(raw_Signal = .data$Signal,
+             Signal = gsub(".*\\((.*)\\).*", "\\1", .data$Signal)) # pattern to extract the names that are inside the parentheses
   }
   repaired_colnames <- tbl_name_time %>%
     unite(col = "Signal_Time", c("Signal","Time"))
   # repair the colnames of the tibble and reframe the tibble to be in long format with signal and time separated
   tidy_data <- data %>%
     rename_with(.fn = ~repaired_colnames$Signal_Time, .cols = repaired_colnames$raw_Signal) %>%
-    filter(Content != "Time [h]")  %>%
+    filter(.data$Content != "Time [h]")  %>%
     pivot_longer(cols = !all_of(discriminatory_variable), names_to = "Signal_Time", values_to = "Value") %>%
     separate(col = c("Signal_Time"), into = c("Signal","Time"), sep = "_", convert = TRUE)
 
