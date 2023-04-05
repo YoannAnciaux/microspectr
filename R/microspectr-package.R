6759e2afa7718d018a5825ebdbde99c5352fe065
YoannAnciaux <anciaux.yoann@gmail.com>
2023-04-05 21:58:18 +0200

    Create depencies - Update Description and Namespace for depencies - Create R file wich contains the import depencies for Roxygen in a centralised way

---
+++ R/microspectr-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @import dplyr
+#' @importFrom readxl read_excel
+#' @importFrom rlang set_names
+#' @importFrom stringr str_replace_all
+#' @importFrom tidyr separate
+## usethis namespace: end
+NULL
