b6ef191fe5f4bbd218c294ffbad66c7641191790
YoannAnciaux <anciaux.yoann@gmail.com>
2023-04-05 21:34:18 +0200

    Correct in microspecter_example Error from copying from readxl doc

--- R/import_data.R
+++ R/import_data.R
@@ -6,12 +6,15 @@
 #' @param path Name of file. If `NULL`, the example files will be listed.
 #' @export
 #' @examples
-#' readxl_example()
-#' readxl_example("Ecoli_T7_24well_Lux_2Fluo_OD.xlsx")
+#' microspectr_example()
+#' microspectr_example("Ecoli_T7_24well_Lux_2Fluo_OD.xlsx")
 microspectr_example <- function(path = NULL) {
   if (is.null(path)) {
-    dir(system.file("extdata", package = "readxl"))
+    dir(system.file("extdata", package = "microspectr"))
   } else {
-    system.file("extdata", path, package = "readxl", mustWork = TRUE)
+    system.file("extdata", path, package = "microspectr", mustWork = TRUE)
   }
 }
+
+microspectr_example()
+microspectr_example("Ecoli_T7_24well_Lux_2Fluo_OD.xlsx")
