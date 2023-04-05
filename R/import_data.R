b1f5917893762bebf4a60dbd7e42bd76efc674bc
YoannAnciaux <anciaux.yoann@gmail.com>
2023-04-06 01:41:56 +0200

    Correct example import_excel

--- R/import_data.R
+++ R/import_data.R
@@ -17,8 +17,8 @@
 #' Signal contains either raw names from *BMG MARS* or new names from `name_repair`.
 #' @export
 #' @examples
-# datasets <- microspectr_example("Ecoli_T7_24well_Lux_2Fluo_OD.xlsx")
-# import_excel(datasets)
+#' datasets <- microspectr_example("Ecoli_T7_24well_Lux_2Fluo_OD.xlsx")
+#' import_excel(datasets)
 #'
 #' # With repaired names
 #' signal_names <- list("No filter 1" = "LUX", "480-20 2" = "CFP", "600-40 3" = "SYTOX", "4" = "OD")
